bea5c6d111ff20b22cddfe6acc34ec03ba8ecfcb
abeslier <Adrien.Beslier@ulb.be>
2025-03-04 14:25:34 +0100

    ServerPartDeals

---
+++ src/websites/serverpartdeals.py
@@ -0,0 +1,8 @@
+from website import Website
+
+
+# https://serverpartdeals.com/collections/everything-seagate?sort=price-ascending&pf_t_interface_type=interface%3ASATA&pf_st_stock_status=true
+
+
+class ServerPartDeals(Website):
+    pass
